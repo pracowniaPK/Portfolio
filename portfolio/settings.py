aceee696e1353efca635012bbb519f110f02af22
pracowniaPK <kaczory5@gmail.com>
2020-08-22 19:17:50 +0200

    add pre-commit

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -102,9 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',  # noqa
     },
-    {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',  # noqa
-    },
+    {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',},  # noqa
     {
         'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',  # noqa
     },
